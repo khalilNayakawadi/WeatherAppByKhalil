4f2e444c0de755396d0de384d91286ba8a79e904
khalilNayakawadi <khalilnayakawadi.01@gmail.com>
2024-02-13 22:49:38 +0530

    js fuction added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getWeatherData(cityName)
+ function getWeatherData(cityName)
 {
-	let url = ""
+	let url = "https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}";
+	let cityName = document.getElementById(city_name);
+	let apiKey = "198420045f8b90d8ed55d50467cf7b94";
+	console.log(url.valueOf)
+	
 }
